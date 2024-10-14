b06d8a066f83c33a75d14bba6f275828ae2f5de8
Valorie Portem <valorieportem@gmail.com>
2024-10-14 22:25:09 +0800

    git commit -m "initial commit"

---
+++ arrayActivity.js
@@ -0,0 +1,16 @@
+const favSingers = [ "Beyoncé","Lee Dokyeom", "Mitchel Cave"]; 
+
+
+console.log(favSingers[0]); 
+
+
+const favNumbers = [7, 14, 21, 8];
+
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+
+console.log(mixedArr[0]); 
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
